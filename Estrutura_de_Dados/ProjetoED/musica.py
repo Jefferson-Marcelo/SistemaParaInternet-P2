7698f03d86a7012429fc7a871cc4260f68aef6ef
Jefferson-Marcelo <jeffersonmarcelo_os@hotmail.com>
2020-11-05 12:51:48 -0300

    musica

--- Estrutura_de_Dados/ProjetoED/musica.py
+++ Estrutura_de_Dados/ProjetoED/musica.py
@@ -1,5 +1,11 @@
 class Musica:
-  def __init__(self, nome, banda, album, genero, anoLancado):
+  def __init__(self):
+      nome = input('digite o nome da música: ')
+      banda = input('digite o nome da banda: ')
+      album = input('digite o nome do album: ')
+      genero = input('digite o nome do genero: ')
+      anoLancado = input('digite o ano do Lançamento da música: ')
+
       self.nome = nome
       self.genero = genero
       self.banda = banda
@@ -9,7 +15,7 @@ class Musica:
   def __str__(self):
     return f'[{self.nome}, {self.genero}, {self.banda}, {self.album}, {self.anoLancado}]'
 
-musica1 = Musica("faroeste", "legiao", "dois" , "rock", 2000 )
-musica2 = Musica("caboclo", "legiao", "dois" , "rock", 2000 )
+musica1 = Musica()
+print(musica1)
 
 
