2a49e2f957d89b650174ccf2dc7ed10109812b97
Alecsander Camilo <alecsandercamilo@gmail.com>
2020-11-05 11:34:03 -0300

    Lista Encadeada
    atualização do Lista Encadeada

--- Estrutura_de_Dados/ProjetoED/listaEncadeada.py
+++ Estrutura_de_Dados/ProjetoED/listaEncadeada.py
@@ -1,198 +1,250 @@
 class ListaException(Exception):
-    def __init__(self, msg):
-        super().__init__(msg)
+  def __init__(self,mensagem):
+    super().__init__(mensagem)
 
+"""## **Classe Nó**"""
 
 class Node:
-    def __init__(self,dado):
-        self.__dado = dado
-        self.__prox = None
+  def __init__(self, dado = None):
+    self.__dado = dado
+    self.__prox = None
+  
+  # get
+  @property
+  def dado(self):
+    return self.__dado
+  
+  # set
+  @dado.setter
+  def dado(self, novo):
+    self.__dado = novo
 
+  # get
+  @property
+  def prox(self):
+    return self.__prox
+  
+  # set
+  @prox.setter
+  def prox(self, novo):
+    self.__prox = novo
 
-    @property
-    def dado(self):
-        return self.__dado
+"""## **Classe Lista Encadeada**"""
 
-    @property
-    def prox(self):
-        return self.__prox
+class ListaEncadeada:
+  def __init__(self):
+    self.__head = None
+    self.__tamanho = 0
 
-    @dado.setter
-    def dado(self, novoDado):
-        self.__dado = novoDado
+  @property
+  def head(self):
+    return self.__head
+  
+  @head.setter
+  def head(self, no):
+    self.__head = no
 
-    @prox.setter
-    def prox(self, novoProx):
-        self.__prox = novoProx
+  def vazia(self):
+    return self.__tamanho == 0
+  
+  def tamanho(self):
+    return self.__tamanho
+  
+  def busca(self, dado):
+    if self.vazia():
+      raise ListaException('A lista está vazia')
 
-    def __str__(self):
-        return str(self.__dado)
+    p = self.__head
+    contador = 1
 
-        
-class Lista:
+    while p != None:
+      if p.dado == dado:
+        return contador
+      
+      p = p.prox
+      contador += 1
 
+    raise ListaException('O valor procurado não está na lista')
+  
+  def elemento(self, posicao):
 
-    def __init__(self):
-        self.__head = None
-        self.__tamanho = 0
+    try:
+      assert posicao > 0
+      
+      if self.vazia():
+          raise ListaException('A lista está vazia')
+      
 
-    def vazia(self):
-        return True if self.__tamanho == 0 else False
+      p = self.__head
+      contador = 1
 
-  # def cheia(self):
-  #     pass
+      # Andar na lista
+      while p != None and contador < posicao:
+        p = p.prox
+        contador += 1
+      
+      # Posição encontrada
+      if p != None:
+        return p.dado
+      
+      raise ListaException('A posição é inválida')
+    
+    except TypeError:
+      raise ListaException('A posição deve ser um valor inteiro')
+    except AssertionError:
+      raise ListaException('Posição negativa não é válida')
+    except:
+      raise
 
-    def tamanho(self):
-        return self.__tamanho
+  def inserir(self, posicao, dado):
+    try:
+      assert posicao > 0
 
-    def busca(self, dado):
-        if (self.vazia()):
-            raise ListaException('A lista esta vazia')
+      # CONDIÇÃO 1: Inserção se a lista estiver vazia
+      if self.vazia():
+        if posicao != 1:
+          raise ListaException('A lista está vazia. Só poderá ser inserido na posição 1')
 
-        cursor = self.__head
-        contador = 1
+        self.__head = Node(dado)
+        self.__tamanho += 1
+        return
+      
+      # CONDIÇÃO 2: Inserção na primeira posição em uma lista não vazia
+      if posicao == 1:
+        novo = Node(dado)
+        novo.prox = self.__head
+        self.__head = novo
+        self.__tamanho += 1
+        return
 
-        while(cursor != None):
-            if (cursor.dado == dado):
-                return contador
+      # CONDIÇÃO 3: Inserção após a primeira posição em uma lista não vazia
+      p = self.__head
+      contador = 1
+      
+      while (contador < posicao-1) and (p != None):
+        p = p.prox
+        contador += 1
 
-            cursor = cursor.prox
-            contador += 1
-        
-        raise ListaException('O valor informado na busca não esta na lista')
+      if p == None:
+        raise ListaException('A posição é inválida para inserção')
+      
+      novo = Node(dado)
+      novo.prox = p.prox
+      p.prox = novo
+      self.__tamanho += 1
 
-    def elemento(self,posicao):
-        #valor negativo:
-        #posição invalida (IndexError)
-        #posição com o tipo de dado diferente de Int TypeError
-        try:
-            assert posicao > 0 #Assegure que posição é maior que zero
-            if (self.vazia()):
-                raise ListaException('A lista esta vazia')
+    except TypeError:
+      raise ListaException('A posição deve ser um valor inteiro')
+    except AssertionError:
+      raise ListaException('Posição negativa não é válida')
+    except:
+      raise
 
-            cursor = self.__head
-            contador = 1
+  def remover(self, posicao):
+    try:
+      assert posicao > 0
 
-            while( (cursor != None) and (contador < posicao)):
-                cursor = cursor.prox
-                contador +=1
+      if self.vazia():
+        raise ListaException('Lista vazia. Não é possível remover elementos')
 
-            if (cursor != None):
-                return cursor.dado
+      p = self.__head
+      contador = 1
 
-            raise ListaException('A procição é invalido para a lista')
-        
-        except TypeError:
-            raise ListaException('Coloque um número inteiro para fazer a busca')        
-        except AssertionError:
-            raise ListaException('Posição negativa não é valida para lista')
-        except:
-            raise
+      while (contador <= posicao-1) and (p != None):
+        anterior = p
+        p = p.prox
+        contador += 1
+      
+      if p == None:
+        raise ListaException('Posição inválida para remoção')
+      
+      dado = p.dado
 
+      if posicao == 1:
+        self.__head = p.prox
+      
+      else:
+        anterior.prox = p.prox
 
-    def inserir(self, posicao, dado):
-        try:
-            assert posicao > 0
-            #CONDIÇÃO 1: Inserção se a lista estiver vazia
+    except TypeError:
+      raise ListaException('A posição deve ser um valor inteiro')
+    except AssertionError:
+      raise ListaException('Posição negativa não é válida')
+    except:
+      raise
+  
+  def __str__(self):
+    saida = 'Lista: ['
+    p = self.__head
 
-            if (self.vazia()):
-                if (posicao != 1):
-                    raise('A lista esta vazia. Defina o argumento da posição como 1')
-                self.__head = Node(dado)
-                self.__tamanho +=1
-                return
-            
-            #CONDIÇÃO 2: Inserção na primeira posição em uma lista vazia
-            if (posicao == 1):
-                novo = Node(dado)
-                novo.prox =  self.__head
-                self.__head = novo
-                self.__tamanho +=1
-                return
+    while p != None:
+      saida += f'{p.dado}'
+      p = p.prox
 
-        except TypeError:
-            raise ListaException('Coloque um número inteiro para fazer a busca')
-        except IndexError:
-            raise ListaException('Esse índice não esta na lista')
-        except AssertionError:
-            raise ListaException('Posição negativa não é valida para lista')
-        except:
-            raise
+      if p != None:
+        saida += ', '
+    
+    saida += ']'
+    return saida
 
+  def imprimir(self):
+    print(self.__str__())
 
-    def remover(self, posicao):
-        try:
-            assert posicao > 0
-            if (self.vazia()):
-                raise ListaException('Lista vazia. Não é possível remover elementos')
-            valor = self.__head[posicao-1]
-            del self.__head[posicao-1]
-            return valor
-        except TypeError:
-            raise ListaException('Coloque um número inteiro para fazer a busca')
-        except IndexError:
-            raise ListaException('Esse índice não esta na lista')
-        except AssertionError:
-            raise ListaException('Posição negativa não é valida para lista')
-        except:
-            raise
+  def modificar(self, posicao, novoValor):
+    try:
+      assert posicao > 0
 
-    def __str__(self):
-        return self.__head.__str__()
+      if self.vazia():
+        raise ListaException('Lista vazia. Não é possível remover elementos')
 
-    def imprimir(self):
-        print(self.__str__())
+      p = self.__head
+      contador = 1
 
-    def modificar(self, posicao, novoValor):
-        try:
-            assert posicao > 0
-            if (self.vazia()):
-                raise ListaException('Lista vazia. Não é possível modificar elementos')
-            self.__head[posicao-1] = novoValor
-            return True
-        except TypeError:
-            raise ListaException('Coloque um número inteiro para fazer a busca')
-        except IndexError:
-            raise ListaException('Esse índice não esta na lista')
-        except AssertionError:
-            raise ListaException('Posição negativa não é valida para lista')
-        except:
-            raise        
-            
-            
-l1 = Lista()
-if (l1.vazia()):
-    print('lista vazia')
+      while (p != None) and (contador < posicao):
+        p = p.prox
+        contador += 1
+      
+      if p != None:
+        p.dado = novoValor
+        return
+      
+      raise ListaException('Posição inválida para a lista')
 
-print('Tamanho: ', l1.tamanho())
+    except TypeError:
+      raise ListaException('A posição deve ser um valor inteiro')
+    except AssertionError:
+      raise ListaException('Posição negativa não é válida')
+    except:
+      raise
 
-for i in range(10):
-    l1.inserir(1, i*10)
-    print(l1)
+if __name__ == '__main__':
+  lista = ListaEncadeada()
+  lista.inserir(1,10)
+  lista.inserir(1,20)
+  lista.inserir(1,30)
 
-print('Tamanho: ', l1.tamanho())
+  print(lista)
 
-valorRemover = l1.remover(20)
-print(valorRemover)
-print(l1)
+  # Inserir o valor 5 entre 30 e 20
+  lista.inserir(2,5)
+  print(lista)
 
-print()
+  # Inserir o valor 12 na última posição
+  lista.inserir(5,12)
+  print(lista)
 
-l1.imprimir()
+  # Modificar o valor da posição 3 (20), para 25
+  lista.modificar(3,25)
+  print(lista)
 
-l1.modificar(3,7)
+  # Remover o elemento da última posição
+  lista.remover(5)
+  print(lista)
 
-l1.imprimir()
+  # Remover o elemento da posição 2
+  lista.remover(2)
+  print(lista)
 
-#l1.inserir(-10,99)
-
-#try:
-#    #print(f'O indice do elemento que esta procurando se encontra na posição: {l1.busca(55)}')
-#    #print(l1.elemento(5))
-#    #l1.inserir(5,111)
-#    #print(l1)
-#    #l1.remover('1')
-#    l1.modificar(100,999)
-#    
-#except ListaException as li:
-#    print(li)
+  # Remover o elemento da posição 100 ==> ERRO ==> COMPLETE O CÓDIGO PARA TRATAR A EXCEÇÃO
+  lista.remover(100)
+  print(lista)
