59d68c63404194dbb7f401a736077146a23f09d6
Alecsander Camilo <alecsandercamilo@gmail.com>
2020-11-06 12:10:44 -0300

    Update musica.py

--- Estrutura_de_Dados/ProjetoED/musica.py
+++ Estrutura_de_Dados/ProjetoED/musica.py
@@ -9,7 +9,14 @@ class Musica:
   def __str__(self):
     return f'[{self.nome}, {self.banda}, {self.album}]'    
 #    return f'[{self.nome}, {self.genero}, {self.banda}, {self.album}, {self.anoLancado}]'
+  
+  def asdict(self):
+    return {'nome': self.nome, 'banda': self.banda, 'album': self.album}
 
 # print(musica1)
 
+musica1 = Musica()
+musica1 = musica1.asdict()
 
+print(musica1)
+print(sorted(musica1))
