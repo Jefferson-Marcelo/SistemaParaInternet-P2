bc99b581592fd59bbb943c24d44c444251b851aa
Alecsander Camilo <alecsandercamilo@gmail.com>
2020-11-07 10:42:09 -0300

    Inicialização

--- Estrutura_de_Dados/ProjetoED/raiz.py
+++ Estrutura_de_Dados/ProjetoED/raiz.py
@@ -5,6 +5,12 @@ from musica import Musica
 from modulo.shutil import print_centralizado
 
 if __name__ == "__main__":
+    print_centralizado('''Saudações da equipe COVID-19!''')
+    print_centralizado('Este programa trabalhará com as 100 maiores músicas brasileiras segundo a Rolling Stones Brasil.')
+    print_centralizado('Para facilidade de visualização, com o uso da biblioteca panda só pegamos as 51 (uma boa ideia) primeiras músicas.')
+    print_centralizado('Os dados da lista são, em ordem normal: Colocação no Ranking, Música, Banda ou Intérprete, Compositores e Ano')
+
+    input("\nPressione ENTER para iniciar...")
     print_centralizado('Início da classe Lista Encadeada.')
     print_centralizado('Serão ADICIONADOS todos os dados sobre as músicas a seguir.')
     input("\nPressione ENTER para continuar...")
