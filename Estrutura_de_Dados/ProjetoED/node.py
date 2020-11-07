20f9a697238b7937809335b715472b24c8d654a8
Alecsander Camilo <alecsandercamilo@gmail.com>
2020-11-07 02:09:08 -0300

    Node correto

--- Estrutura_de_Dados/ProjetoED/node.py
+++ Estrutura_de_Dados/ProjetoED/node.py
@@ -1,31 +1,24 @@
-import musica
-
-
 class Node:
-  def __init__(self, data = None):    
-    self.__data = data
-    self.__next = None
-  
-  # get
-  @property
-  def data(self):
-    return self.__data
-  
-  # set
-  @data.setter
-  def data(self, newData):
-    self.__data = newData
+    def __init__(self, data=None):
+        self.__data = data
+        self.__next = None
 
-  # get
-  @property
-  def next(self):
-    return self.__next
-  
-  # set
-  @next.setter
-  def next(self, newData):
-    self.__next = newData
+    # get
+    @property
+    def data(self):
+        return self.__data
 
-# no1 = Node(musica.musica1)
-#print(no1.data.nome)
-#print(no1.data)
+    # set
+    @data.setter
+    def data(self, newData):
+        self.__data = newData
+
+    # get
+    @property
+    def next(self):
+        return self.__next
+
+    # set
+    @next.setter
+    def next(self, newData):
+        self.__next = newData
