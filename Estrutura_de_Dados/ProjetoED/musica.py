3695d0472dabc7666b07f45037249a7a16f70180
Alecsander Camilo <alecsandercamilo@gmail.com>
2020-11-07 02:08:42 -0300

    Update musica.py

--- Estrutura_de_Dados/ProjetoED/musica.py
+++ Estrutura_de_Dados/ProjetoED/musica.py
@@ -1,17 +1,41 @@
 class Musica:
-  def __init__(self):
-      self.nome = input('digite o nome da música: ')
-      self.banda = input('digite o nome da banda: ')
-      self.album = input('digite o nome do album: ')
-    #  self.genero = input('digite o nome do genero: ')
-    #  self.anoLancado = input('digite o ano do Lançamento da música: ')
- 
-  def __repr__(self):    
-    return f'[{self.nome}, {self.banda}, {self.album}]'    
-#    return f'[{self.nome}, {self.genero}, {self.banda}, {self.album}, {self.anoLancado}]'
+    def __init__(self, nome, banda, album, ano):
+        self.__nome = nome
+        self.__banda = banda
+        self.__album = album
+        self.__ano = ano
 
-  
+    @property
+    def nome(self):
+        return self.__nome
 
+    @nome.setter
+    def nome(self, novoNome):
+        self.__nome = novoNome
 
-# print(musica1)
+    @property
+    def banda(self):
+        return self.__nome
 
+    @banda.setter
+    def banda(self, novaBanda):
+        self.__nome = novaBanda
+
+    @property
+    def album(self):
+        return self.__album
+
+    @album.setter
+    def album(self, novoAlbum):
+        self.__album = novoAlbum
+
+    @property
+    def ano(self):
+        return self.__ano
+
+    @ano.setter
+    def ano(self, novoAno):
+        self.__ano = novoAno
+
+    def __repr__(self):
+        return f'[{self.__nome}, {self.__banda}, {self.__album}, {self.__ano}]\n'
